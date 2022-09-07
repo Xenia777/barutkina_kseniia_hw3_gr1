ddb9b4dc1f0d9daa813d425f647411a8d7478d75
Xenia777 <109750310+Xenia777@users.noreply.github.com>
2022-09-07 12:24:25 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
-        public static void main(String[] args) {
+        public static void Solution(String[] args) {
             double[] fractionalNumbers = {1.3, 8.6, 5, -3.1, -8.5, 4.2, -3.2, -1.3, 4.4, -2.6, 9.1, -2,2, 5.3, -9.4, 6.1};
             double sum = 0.0;
             int count = 0;
